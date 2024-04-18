fdeac43452f8ec06056c34f0a73172aed0f28d41
ethan <20230006@ricaldone.edu.sv>
2024-04-18 10:32:22 -0600

    lo subi

---
+++ app/src/main/java/ethan/herniquez/practicaethan/activity_login.kt
@@ -0,0 +1,32 @@
+package ethan.herniquez.practicaethan
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnsiguientePantalla =findViewById<Button>(R.id.btnSiguiente)
+
+        btnsiguientePantalla.setOnClickListener {
+
+            val siguiente = Intent(this, MainActivity::class.java)
+            startActivity(siguiente)
+
+        }
+
+    }
+}
